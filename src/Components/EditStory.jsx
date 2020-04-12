3bad85353007a1e3214cc8d39e21f49e38f5badf
Civan Erbay <Civan.Erbay@questback.com>
2020-04-12 13:01:35 +0200

    frontend resultpage and some changes in es and cs

--- src/Components/EditStory.jsx
+++ src/Components/EditStory.jsx
@@ -7,176 +7,229 @@ import { Link } from "react-router-dom";
 import { Row, Col, Container } from "react-bootstrap";
 
 export default class EditStory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      isError: false,
-      hintText: "",
-      storyId: "",
-      secret: "",
-      story: {},
-      submitSuccess: false,
-      nextLink: "",
-    };
-  }
-
-  componentDidMount() {
-    const {
-      match: { params },
-    } = this.props;
-
-    const storyId = params.storyId;
-    const secret = params.secret;
-    this.getStory(storyId);
-    if (secret) {
-      this.setState({ storyId, secret });
-    } else {
-      this.setState({ storyId });
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            isError: false,
+            hintText: "",
+            storyId: "",
+            secret: "",
+            story: {},
+            submitSuccess: false,
+            nextLink: "",
+        };
     }
-  }
 
-  async getStory(storyId) {
-    let storyRef = await db
-      .collection("stories")
-      .doc(storyId)
-      .get()
-      .catch(function (error) {
-        console.log("Error getting document:", error);
-        this.setState({ error: true });
-      });
+    componentDidMount() {
+        const {
+            match: { params },
+        } = this.props;
 
-    if (storyRef.exists) {
-      const story = storyRef.data();
-      let hintText = story.storyParts.reduce((acc, part) => acc + part.text + " ", "");
-      if (hintText.length > 200) hintText = hintText.substring(hintText.length - 100, hintText.length - 1);
-      // check that secret is valid
-      const validParticipant = story.participants.filter(
-        (participant) => participant.secret && this.state.secret === participant.secret
-      );
-      this.setState({ story, isLoading: false, hintText, validParticipant });
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-      this.setState({ error: true });
+        const storyId = params.storyId;
+        const secret = params.secret;
+        this.getStory(storyId);
+        if (secret) {
+            this.setState({ storyId, secret });
+        } else {
+            this.setState({ storyId });
+        }
     }
-  }
 
-  checkSecret() {
-    // get secret param from URLs
-  }
+    async getStory(storyId) {
+        let storyRef = await db
+            .collection("stories")
+            .doc(storyId)
+            .get()
+            .catch(function (error) {
+                console.log("Error getting document:", error);
+                this.setState({ error: true });
+            });
 
-  handleChange(event) {
-    let fieldName = event.target.name;
-    let fleldVal = event.target.value;
-    this.setState({ ...this.state, [fieldName]: fleldVal });
-  }
+        if (storyRef.exists) {
+            const story = storyRef.data();
+            let hintText = story.storyParts.reduce(
+                (acc, part) => acc + part.text + " ",
+                ""
+            );
+            if (hintText.length > 200)
+                hintText = hintText.substring(
+                    hintText.length - 100,
+                    hintText.length - 1
+                );
+            // check that secret is valid
+            const validParticipant = story.participants.filter(
+                (participant) =>
+                    participant.secret &&
+                    this.state.secret === participant.secret
+            );
+            this.setState({
+                story,
+                isLoading: false,
+                hintText,
+                validParticipant,
+            });
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            this.setState({ error: true });
+        }
+    }
 
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    const { storyId, story, storyText } = this.state;
-    let storyRef = db.collection("stories").doc(storyId);
+    checkSecret() {
+        // get secret param from URLs
+    }
 
-    const newStoryPart = {
-      // author: creatorEmail,
-      timestamp: new Date(),
-      text: storyText,
+    handleChange(event) {
+        let fieldName = event.target.name;
+        let fleldVal = event.target.value;
+        this.setState({ ...this.state, [fieldName]: fleldVal });
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const { storyId, story, storyText } = this.state;
+        let storyRef = db.collection("stories").doc(storyId);
+
+        const newStoryPart = {
+            // author: creatorEmail,
+            timestamp: new Date(),
+            text: storyText,
+        };
+
+        story.storyParts.push(newStoryPart);
+
+        await storyRef
+            .set({ ...story }, { merge: true })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+                this.setState({ error: true });
+                return;
+            });
+        // TODO look for next participant to show their email and generate secret link
+        const nextParticipant = { email: "dummy!" };
+        // TODO consider end of story
+        this.setState({
+            nextLink: `www.tellzy.web.app/story/${storyId}/${nextParticipant.secret}`,
+            storyLink: `www.tellzy.web.app/story/${storyId}`,
+            submitSuccess: true,
+            nextParticipant,
+        });
     };
 
-    story.storyParts.push(newStoryPart);
+    render() {
+        const {
+            nextLink,
+            submitSuccess,
+            story,
+            isLoading,
+            hintText,
+            secret,
+            nextParticipant,
+            storyLink,
+        } = this.state;
 
-    await storyRef.set({ ...story }, { merge: true }).catch(function (error) {
-      console.error("Error adding document: ", error);
-      this.setState({ error: true });
-      return;
-    });
-    // TODO look for next participant to show their email and generate secret link
-    const nextParticipant = { email: "dummy!" };
-    // TODO consider end of story
-    this.setState({
-      nextLink: `www.tellzy.web.app/story/${storyId}/${nextParticipant.secret}`,
-      storyLink: `www.tellzy.web.app/story/${storyId}`,
-      submitSuccess: true,
-      nextParticipant,
-    });
-  };
+        return (
+            <div className="edit-story">
+                <Row className="my-5">
+                    <Col sm={2}>
+                        <Link to="/">
+                            <Button className="btn-home">Home</Button>
+                        </Link>
+                    </Col>
+                    <Col sm={8} className="h-100">
+                        {isLoading ? (
+                            <p>Loading...</p>
+                        ) : (
+                            <>
+                                {submitSuccess ? (
+                                    <LinkPage
+                                        nextLink={nextLink}
+                                        nextParticipant={nextParticipant}
+                                        storyLink={storyLink}
+                                    ></LinkPage>
+                                ) : (
+                                    <>
+                                        <h1 className="h1-es-false text-center">
+                                            {story.storyTitle}
+                                        </h1>
+                                        {}
+                                        {/* STAT STORY */}
+                                        {secret ? (
+                                            <Form
+                                                className="form-es-false"
+                                                onSubmit={this.handleSubmit}
+                                            >
+                                                <Form.Group>
+                                                    <Form.Label>
+                                                        Previously on "
+                                                        {story.storyTitle}"...
+                                                    </Form.Label>
+                                                    <Form.Control
+                                                        readOnly
+                                                        as="textarea"
+                                                        value={hintText}
+                                                        rows="2"
+                                                        name="hintText"
+                                                    />
+                                                </Form.Group>
+                                                <Form.Group>
+                                                    <Form.Label>
+                                                        Continue your Story
+                                                    </Form.Label>
+                                                    <Form.Control
+                                                        required
+                                                        as="textarea"
+                                                        placeholder="Continue the adventure..."
+                                                        rows="10"
+                                                        name="storyText"
+                                                        onChange={this.handleChange.bind(
+                                                            this
+                                                        )}
+                                                    />
+                                                </Form.Group>
 
-  render() {
-    const {
-      nextLink,
-      submitSuccess,
-      story,
-      isLoading,
-      hintText,
-      secret,
-      nextParticipant,
-      storyLink,
-    } = this.state;
-
-    return (
-      <Container className="edit-story">
-        <Row className="my-5">
-          <Col sm={2}>
-            <Link to="/">
-              <Button className="btn-home">Home</Button>
-            </Link>
-          </Col>
-          <Col sm={8} className="h-100">
-            {isLoading ? (
-              <p>Loading...</p>
-            ) : (
-              <>
-                {submitSuccess ? (
-                  <LinkPage
-                    nextLink={nextLink}
-                    nextParticipant={nextParticipant}
-                    storyLink={storyLink}
-                  ></LinkPage>
-                ) : (
-                  <>
-                    <h1>{story.storyTitle}</h1>
-                    {}
-                    {/* STAT STORY */}
-                    {secret ? (
-                      <Form onSubmit={this.handleSubmit}>
-                        <Form.Group>
-                          <Form.Label>Previously on "{story.storyTitle}"...</Form.Label>
-                          <Form.Control readOnly as="textarea" value={hintText} rows="2" name="hintText" />
-                        </Form.Group>
-                        <Form.Group>
-                          <Form.Label>Continue your Story!</Form.Label>
-                          <Form.Control
-                            required
-                            as="textarea"
-                            placeholder="Once upon a time..."
-                            rows="10"
-                            name="storyText"
-                            onChange={this.handleChange.bind(this)}
-                          />
-                        </Form.Group>
-
-                        <Button type="submit">Submit story</Button>
-                      </Form>
-                    ) : (
-                      <div>
-                        <p>Here are the participants:</p>
-                        <ul>
-                          {story.participants.map((participant) => (
-                            <li key={participant.email}>
-                              {participant.email} - {participant.isSubmitted ? "Submitted" : "Todo"}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
-                  </>
-                )}
-              </>
-            )}
-          </Col>
-          <Col sm={2}></Col>
-        </Row>
-      </Container>
-    );
-  }
+                                                <Button
+                                                    className="go-btn-cs-false"
+                                                    type="submit"
+                                                >
+                                                    Submit
+                                                </Button>
+                                            </Form>
+                                        ) : (
+                                            <div>
+                                                <p>
+                                                    Here are the participants:
+                                                </p>
+                                                <ul>
+                                                    {story.participants.map(
+                                                        (participant) => (
+                                                            <li
+                                                                key={
+                                                                    participant.email
+                                                                }
+                                                            >
+                                                                {
+                                                                    participant.email
+                                                                }{" "}
+                                                                -{" "}
+                                                                {participant.isSubmitted
+                                                                    ? "Submitted"
+                                                                    : "Todo"}
+                                                            </li>
+                                                        )
+                                                    )}
+                                                </ul>
+                                            </div>
+                                        )}
+                                    </>
+                                )}
+                            </>
+                        )}
+                    </Col>
+                    <Col sm={2}></Col>
+                </Row>
+            </div>
+        );
+    }
 }
--- src/Components/ResultStory.jsx
+++ src/Components/ResultStory.jsx
@@ -2,66 +2,87 @@ import React, { Component } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { db } from "./../config/firebaseConfig";
-
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 export default class ResultStory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      isError: false,
-      storyFinished: false,
-      story: {},
-    };
-  }
-
-  componentDidMount() {
-    const {
-      match: { params },
-    } = this.props;
-
-    const storyId = params.storyId;
-    this.getStory(storyId);
-    this.setState({ storyId });
-  }
-
-  async getStory(storyId) {
-    let storyRef = await db
-      .collection("stories")
-      .doc(storyId)
-      .get()
-      .catch(function (error) {
-        console.log("Error getting document:", error);
-        this.setState({ error: true });
-      });
-
-    if (storyRef.exists) {
-      const story = storyRef.data();
-      const storyFinished = story.participants.every((participant) => participant.isSubmitted);
-      this.setState({ story, isLoading: false, storyFinished });
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-      this.setState({ error: true });
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            isError: false,
+            storyFinished: false,
+            story: {},
+        };
     }
-  }
 
-  render() {
-    // story = {storyTitle, participants: [{email, isSubmitted}], storyParts: [{author, text}]}
-    const { story, isLoading, storyFinished } = this.state;
+    componentDidMount() {
+        const {
+            match: { params },
+        } = this.props;
 
-    return (
-      <div>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          <div className="d-flex flex-column justify-content-center align-items-center mt-3">
-            <p>{storyFinished ? "The story is finished" : "Still waiting for a participant"}</p>
-            <textarea className="w-50 h-50">
-              {story.storyParts.reduce((acc, curr) => acc + curr.text + "\n", "")}
-            </textarea>
-          </div>
-        )}
-      </div>
-    );
-  }
+        const storyId = params.storyId;
+        this.getStory(storyId);
+        this.setState({ storyId });
+    }
+
+    async getStory(storyId) {
+        let storyRef = await db
+            .collection("stories")
+            .doc(storyId)
+            .get()
+            .catch(function (error) {
+                console.log("Error getting document:", error);
+                this.setState({ error: true });
+            });
+
+        if (storyRef.exists) {
+            const story = storyRef.data();
+            const storyFinished = story.participants.every(
+                (participant) => participant.isSubmitted
+            );
+            this.setState({ story, isLoading: false, storyFinished });
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            this.setState({ error: true });
+        }
+    }
+
+    render() {
+        // story = {storyTitle, participants: [{email, isSubmitted}], storyParts: [{author, text}]}
+        const { story, isLoading, storyFinished } = this.state;
+
+        return (
+            <div className="w-100">
+                <Row className="whatthefuck">
+                    <Col>
+                        <Link to="/">
+                            <Button className="btn-home">Home</Button>
+                        </Link>
+                    </Col>
+                    <Col sm={8} className="h-100 m-0">
+                        {isLoading ? (
+                            <p>Loading...</p>
+                        ) : (
+                            <div className="d-flex flex-column justify-content-center align-items-center result-true-div">
+                                <h1 className="h1-result-true">Result</h1>
+                                <p>
+                                    {storyFinished
+                                        ? "The story is finished"
+                                        : "Still waiting for a participant"}
+                                </p>
+                                <textarea className="w-75 result-true-text">
+                                    {story.storyParts.reduce(
+                                        (acc, curr) => acc + curr.text + "\n",
+                                        ""
+                                    )}
+                                </textarea>
+                            </div>
+                        )}
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </div>
+        );
+    }
 }
