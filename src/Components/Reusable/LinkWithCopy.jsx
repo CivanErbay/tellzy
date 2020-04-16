681f24f5fabf5b98e6b7bedc09daab5340505aa9
Paco Croket <croketproduction@gmail.com>
2020-04-16 22:54:27 +0200

    reactive copy btn

--- src/Components/Reusable/LinkWithCopy.jsx
+++ src/Components/Reusable/LinkWithCopy.jsx
@@ -3,18 +3,30 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Row } from "react-bootstrap";
 
 export default class LinkWithCopy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCopied: false,
+    };
+  }
+
+  setCopied = () => {
+    this.setState({ isCopied: true });
+  };
+
   render() {
-    const { link, text, isUnfold, setUnfold } = this.props;
+    const { isCopied } = this.state;
+    const { link, text } = this.props;
     return (
       <div className="d-flex flex-column justify-content-center align-items-center my-4">
-        <CopyToClipboard className="clipboard" text={link} onCopy={setUnfold}>
+        <CopyToClipboard className="clipboard" text={link} onCopy={this.setCopied}>
           <span>
             <Row className="row-cs-true p-3">
               <div className="d-flex flex-column align-items-center">
                 <p className="mr-3 my-auto copy-cs-true">{text}</p>
-                {isUnfold ? <>Copied!</> : <>Copy me</>}
+                {isCopied ? <>Copied!</> : <>Copy me</>}
               </div>
-              {isUnfold ? <i className="fas fa-check fa-3x"></i> : <i className="far fa-copy fa-3x"></i>}
+              {isCopied ? <i className="fas fa-check fa-3x"></i> : <i className="far fa-copy fa-3x"></i>}
               {/* <img src="assets/images/copy.png" /> */}
               {this.props.children}
             </Row>
