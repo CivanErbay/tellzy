be4b7884233680611e83ba6d0f8f6b3895f24fff
Paco Croket <croketproduction@gmail.com>
2020-04-17 18:30:29 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import Policy from "./Components/Policy";
+import Policy from "./Components/Reusable/Policy";
 import Contact from "./Components/Contact";
 import Landing from "./Components/Landing";
 import CreatingStory from "./Components/CreatingStory";
@@ -12,30 +10,23 @@ import EditStory from "./Components/EditStory";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return (
-        <div className="App">
-            {/* <header className="App-header">
+  return (
+    <div className="App">
+      {/* <header className="App-header">
         <Navbar></Navbar>
       </header> */}
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/story" component={CreatingStory} />
-                    <Route
-                        exact
-                        path="/story/:storyId"
-                        component={ResultStory}
-                    />
-                    <Route
-                        path="/story/:storyId/:secret"
-                        component={EditStory}
-                    />
-                    <Route path="/policy" component={Policy} />
-                    <Route path="/contact" component={Contact} />
-                </Switch>
-            </Router>
-        </div>
-    );
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/story" component={CreatingStory} />
+          <Route exact path="/story/:storyId" component={ResultStory} />
+          <Route path="/story/:storyId/:secret" component={EditStory} />
+          <Route path="/policy" component={Policy} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
