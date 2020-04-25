9e204391469e99ce90b1b737c7519c9db958fb8d
Civan Erbay <Civan.Erbay@questback.com>
2020-04-25 21:46:05 +0200

    style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,91 +11,107 @@ import SignInScreen from "./Reusable/SignInScreen";
 import "../styling/navBar.css";
 
 export default class NavBar extends Component {
-  state = {
-    showSignInScreen: false,
-    user: null,
-  };
+    state = {
+        showSignInScreen: false,
+        user: null,
+    };
 
-  componentDidMount = () => {
-    auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        this.setState({ user });
-        // let userData = await getUserData(user.uid);
-        // this.setState({ userData });
-      } else {
-        this.setState({ user, userData: null });
-      }
-    });
-  };
+    componentDidMount = () => {
+        auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                this.setState({ user });
+                // let userData = await getUserData(user.uid);
+                // this.setState({ userData });
+            } else {
+                this.setState({ user, userData: null });
+            }
+        });
+    };
 
-  handleClose = () => this.setState({ showSignInScreen: false });
-  handleShow = () => this.setState({ showSignInScreen: true });
+    handleClose = () => this.setState({ showSignInScreen: false });
+    handleShow = () => this.setState({ showSignInScreen: true });
 
-  render() {
-    const { showSignInScreen } = this.state;
+    render() {
+        const { showSignInScreen } = this.state;
 
-    const signInModal = (
-      <Modal show={showSignInScreen} onHide={this.handleClose}>
-        <Modal.Body>
-          <SignInScreen />
-        </Modal.Body>
-      </Modal>
-    );
+        const signInModal = (
+            <Modal show={showSignInScreen} onHide={this.handleClose}>
+                <Modal.Body>
+                    <SignInScreen />
+                </Modal.Body>
+            </Modal>
+        );
 
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto w-100 text-center">
-            <Col sm={4} className="d-flex flex-row justify-content-end">
-              <Nav.Link href="/#home">Home</Nav.Link>
-              <Nav.Link href="/#how-to">How To</Nav.Link>
-            </Col>
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto w-100 text-center">
+                        <Col
+                            sm={4}
+                            className="d-flex justify-content-center align-items-center"
+                        >
+                            <Nav.Link href="/#home">Home</Nav.Link>
+                            <Nav.Link href="/#how-to">How To</Nav.Link>
+                        </Col>
 
-            <Col sm={4}>
-              <Navbar.Brand href="/#home" className="sm-hide">
-                TELLZY
-              </Navbar.Brand>
-            </Col>
-            <Col lg={4} className="d-flex flex-row justify-content-start">
-              {auth.currentUser ? (
-                <>
-                  <Nav.Link className="disabledCursor" href="#social">
-                    Social
-                  </Nav.Link>
-                  <Nav.Link href="/main#home">Overview</Nav.Link>
-                  <OverlayTrigger
-                    trigger="click"
-                    placement="bottom"
-                    overlay={
-                      <Popover>
-                        <Popover.Title as="h3">Sure?</Popover.Title>
-                        <Popover.Content>
-                          <Button onClick={signOut} href="/#home">
-                            Log Out
-                          </Button>
-                        </Popover.Content>
-                      </Popover>
-                    }
-                  >
-                    <Nav.Link>Log Out</Nav.Link>
-                  </OverlayTrigger>
-                </>
-              ) : (
-                <>
-                  <Nav.Link href="" onClick={this.handleShow}>
-                    Log In
-                  </Nav.Link>
-                  <Nav.Link href="" onClick={this.handleShow}>
-                    Register
-                  </Nav.Link>
-                </>
-              )}
-            </Col>
-          </Nav>
-        </Navbar.Collapse>
-        {signInModal}
-      </Navbar>
-    );
-  }
+                        <Col sm={4}>
+                            <Navbar.Brand href="/#home" className="sm-hide">
+                                TELLZY
+                            </Navbar.Brand>
+                        </Col>
+                        <Col
+                            lg={4}
+                            className="d-flex justify-content-center align-items-center"
+                        >
+                            {auth.currentUser ? (
+                                <>
+                                    <Nav.Link
+                                        className="disabledCursor"
+                                        href="#social"
+                                    >
+                                        Social
+                                    </Nav.Link>
+                                    <Nav.Link href="/main#home">
+                                        Overview
+                                    </Nav.Link>
+                                    <OverlayTrigger
+                                        trigger="click"
+                                        placement="bottom"
+                                        overlay={
+                                            <Popover>
+                                                <Popover.Title as="h3">
+                                                    Sure?
+                                                </Popover.Title>
+                                                <Popover.Content>
+                                                    <Button
+                                                        onClick={signOut}
+                                                        href="/#home"
+                                                    >
+                                                        Log Out
+                                                    </Button>
+                                                </Popover.Content>
+                                            </Popover>
+                                        }
+                                    >
+                                        <Nav.Link>Log Out</Nav.Link>
+                                    </OverlayTrigger>
+                                </>
+                            ) : (
+                                <>
+                                    <Nav.Link href="" onClick={this.handleShow}>
+                                        Log In
+                                    </Nav.Link>
+                                    <Nav.Link href="" onClick={this.handleShow}>
+                                        Register
+                                    </Nav.Link>
+                                </>
+                            )}
+                        </Col>
+                    </Nav>
+                </Navbar.Collapse>
+                {signInModal}
+            </Navbar>
+        );
+    }
 }
--- src/Components/Reusable/SignInScreen.jsx
+++ src/Components/Reusable/SignInScreen.jsx
@@ -3,15 +3,15 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import auth, { uiConfig } from "../../actions/auth";
 
 export default class SignInScreen extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className="brand">Tellzy</h1>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+    render() {
+        return (
+            <div className="signin-div text-center">
+                <h1 className="brand mb-5">Tellzy</h1>
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
 
-        {/* <div id="firebaseui-auth-container"></div>
+                {/* <div id="firebaseui-auth-container"></div>
         <div id="loader">Loading...</div> */}
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
