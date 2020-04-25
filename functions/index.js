bc75cff21efa8a049e346022e7275f89db395fe9
Paco Croket <croketproduction@gmail.com>
2020-04-25 19:57:36 +0200

    function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +6,19 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createUserDoc = functions.auth.user().onCreate((user) => {
+  const userDoc = {
+    uid: user.uid,
+    displayName: "",
+    language: "",
+    storiesStarted: [],
+    storiesParticipant: [],
+    storiesOpen: [],
+    lastLogIn: new Date(),
+    friends: [],
+    points: 0,
+    achievements: [],
+  };
+  const userDocRef = admin.firestore().collection("users").doc(user.uid).set(userDoc);
+});
