c2d5ca1b3a77441c287bf77d5a50c4656d668b8c
Civan Erbay <Civan.Erbay@questback.com>
2020-04-25 15:55:06 +0200

    NavBar - first version

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,36 +2,63 @@ import React, { Component } from "react";
 import { auth } from "../config/firebaseConfig";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import "../styling/navBar.css";
 
 export default class NavBar extends Component {
-  render() {
-    console.log(auth.currentUser);
+    render() {
+        console.log(auth.currentUser);
 
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">How To</Nav.Link>
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto w-100 text-center">
+                        <Col sm={4}>
+                            <Row className="justify-content-center">
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#link">How To</Nav.Link>
+                            </Row>
+                        </Col>
 
-            <Navbar.Brand href="#home">TELLZY style me</Navbar.Brand>
-            {auth.currentUser ? (
-              <>
-                <Nav.Link href="#home">Social</Nav.Link>
-                <Nav.Link href="#home">Profile</Nav.Link>
-                <Nav.Link href="#link">Log Out</Nav.Link>
-              </>
-            ) : (
-              <>
-                <Nav.Link href="#home">Log In</Nav.Link>
-                <Nav.Link href="#link">Register</Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+                        <Col sm={4}>
+                            <Navbar.Brand href="#home">TELLZY</Navbar.Brand>
+                        </Col>
+                        {auth.currentUser ? (
+                            <>
+                                <Col lg={4}>
+                                    <Row className="justify-content-around">
+                                        <Nav.Link
+                                            className="disabledCursor"
+                                            href="#home"
+                                        >
+                                            Social
+                                        </Nav.Link>
+                                        <Nav.Link href="#home">
+                                            Profile
+                                        </Nav.Link>
+                                        <Nav.Link href="#link">
+                                            Log Out
+                                        </Nav.Link>
+                                    </Row>
+                                </Col>
+                            </>
+                        ) : (
+                            <>
+                                <Col sm={4}>
+                                    <Row className="justify-content-center">
+                                        <Nav.Link href="#home">Log In</Nav.Link>
+                                        <Nav.Link href="#link">
+                                            Register
+                                        </Nav.Link>
+                                    </Row>
+                                </Col>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 }
